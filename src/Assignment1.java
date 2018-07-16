5cc69e15c2cc0d157011e16ff286a74bb894d562
Neil Gandhi <neil.gandhi@rutgers.edu>
2018-07-16 18:32:45 -0400

    Draft1

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -1,5 +1,16 @@
 public class Assignment1 {
 	public static void main(String[] args) {
+		String firstName = "Neil";
+		String lastName = "Gandhi";
+		char empty = ' ';
+		char comma = ',';
+		System.out.println(firstName + "\n" + lastName);
+		System.out.println(firstName + empty + lastName);
+		System.out.println(lastName + comma + empty + firstName);
+		System.out.println(firstName.toUpperCase());
+		System.out.println(lastName.toLowerCase());
+		System.out.println(firstName.charAt(2));
+		System.out.println(lastName.charAt(1));
 		
 	}
 }
