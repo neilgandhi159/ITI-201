81f84f65b974dc487915618db65024e1bb3f8974
Neil Gandhi <neil.gandhi@rutgers.edu>
2018-07-18 13:14:18 -0400

    JavaLecture2

---
+++ src/JavaLecture2.java
@@ -0,0 +1,13 @@
+
+public class JavaLecture2 {
+	public static void main(String[] args) {
+		int[] myInts = new int [6]; //this array can hold up to 6 integers indexed 0-5
+
+		// when you know exactly when you will start and stop
+		// the first part tells Java where to start
+		// the second part is telling Java where to stop
+		for(int i = 0; i < myInts.length; i++){
+
+		}//end of for loop
+	}//end of main method
+}//end of JavaLecture2
